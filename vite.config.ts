571223e1e920299673c1c721f48451cd92d7b310
youtube <youtube@Camerons-Mac-mini.local>
2024-01-26 17:06:06 -0600

    feat: add vite-plugin-singlefile for final build upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import path from "node:path";
 import { defineConfig } from "vite";
+import { viteSingleFile } from "vite-plugin-singlefile";
 
 export default defineConfig({
+  plugins: [viteSingleFile()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src")
     }
+  },
+  build: {
+    cssCodeSplit: false,
+    assetsInlineLimit: 100000000
   }
 });
